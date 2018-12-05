c0ea96c79f9e621c7eeda51e7e2a0b38e3ae0c5e
Tyler Branham <branham50129@mvctc.com>
2018-12-05 12:56:06 -0500

    moved the background color to be less invasive and created a filtering system

--- website/js/timeline.js
+++ website/js/timeline.js
@@ -1,35 +1,47 @@
 var headerbox = document.getElementById("infoHere");
 var eventbox = document.getElementById("main");
-var btn = document.getElementById("btn");
-var testBox = document.getElementById("test");
-var counter = 0;
 
+var btnComm = document.getElementById("comm");
+var btnDay = document.getElementById("day");
+var btnEarthR = document.getElementById("earthR");
+var btnEarthT = document.getElementById("earthT");
+var btnMarsTAR = document.getElementById("marsTAR");
+var btnCDR = document.getElementById("CDR");
+var btnCommE = document.getElementById("commE");
+var btnFE1 = document.getElementById("FE1");
+var btnFE2 = document.getElementById("FE2");
+var btnFE3 = document.getElementById("FE3");
+var btnTech1 = document.getElementById("TECH1");
+var btnTech2 = document.getElementById("TECH2");
+var btnMCC = document.getElementById("MCC");
+var btnST = document.getElementById("ST");
+
+var fileData;
 
 
 fetch ('https://cors-anywhere.herokuapp.com/http://gw2.mvctc.com/Class2019/plan.json')
 	.then(res =>res.json())
-	.then ((data) => {
+	.then ((fileData) => {
 		
-		//renderHeader(data);
-		renderEvents(data);
-	/* 	btn.addEventListener("click", function(){
-			renderEvents(data.activities[counter]);//for button
-			//renderEvents(data);//for all
-			
-			counter++;
-	
-		}); */
+		renderEvents(fileData);
+		
+		btnFE1.onclick = function(){sortEvents(fileData, "FE1")};
+		btnFE2.onclick = function(){sortEvents(fileData, "FE2")};
+		btnFE3.onclick = function(){sortEvents(fileData, "FE3")};
+		btnTech1.onclick = function(){sortEvents(fileData, "TECH1")};
+		btnTech2.onclick = function(){sortEvents(fileData, "TECH2")};
+		btnMCC.onclick = function(){sortEvents(fileData, "MCCCOORD")};
+		btnST.onclick = function(){sortEvents(fileData, "ST")};
+		btnComm.onclick = function(){sortEvents(fileData, "COMM_LATENCY")};
+		btnDay.onclick = function(){sortEvents(fileData, "DayNight")};
+		btnEarthR.onclick = function(){sortEvents(fileData, "EARTH_RECEIVE")};
+		btnEarthT.onclick = function(){sortEvents(fileData, "EARTH_TRANSMIT")};
+		btnMarsTAR.onclick = function(){sortEvents(fileData, "MARS_TRANSMIT_AND_RECEIVE")};
+		btnCDR.onclick = function(){sortEvents(fileData, "CDR")};
+		btnCommE.onclick = function(){sortEvents(fileData, "true")};
+		
 	}).catch(err => console.error(err));
 
-
-		
-function renderHeader(file){
-	var rawTemp = document.getElementById("headerTemp").innerHTML;
-	var compTemp = Handlebars.compile(rawTemp);
-	var genHTML = compTemp(file);
-	
-	headerbox.innerHTML = genHTML;
-}
 
 function renderEvents(file){
 	var rawTemp = document.getElementById("eventTemp").innerHTML;
@@ -39,3 +51,24 @@ function renderEvents(file){
 	eventbox.innerHTML = genHTML;
 }
 
+
+function sortEvents(file, sorter) {
+
+	var rows; 
+	var id;
+	
+	for(var i = 0; i < file.activities.length; i++){
+		rows = file.activities[i].rows;
+		id = document.getElementById(file.activities[i].id);
+		for(var j = 0; j < rows.length; j++){
+			if(rows[j] == sorter){
+				id.style.display = "block";
+				break;
+			}
+			else{
+				id.style.display = "none";
+			}
+		}
+	}
+	
+}
